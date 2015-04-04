47236bcfbf0bcf0f1aa65e8f03c4367397b5296a
Kamil Jarosz <kjarosz19946@gmail.com>
2015-04-04 18:44:06 -0400

    Add settings structure.

---
+++ src/mosaicgenerator/utils/Settings.java
@@ -0,0 +1,11 @@
+package mosaicgenerator.utils;
+
+public class Settings {
+   public int cellWidth;
+   public int cellHeight;
+   public int tileWidth;
+   public int tileHeight;
+   
+   public boolean reuseTiles;
+   public int reusePenalty;
+}
